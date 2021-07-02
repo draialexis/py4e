46c11b1783b71bf8f020995c7b3d131be7cc28ce
Alexis DRAI <drai.alexis@gmail.com>
2021-07-02 19:14:51 +0200

    Add new conversion formula, give choice

--- c_to_f.py
+++ c_to_f.py
@@ -1,3 +1,13 @@
-c_temp = float(input('Enter a temperature in Celsius: '))
-f_temp = c_temp * 9 / 5 + 32
-print(f_temp)
+choice = input("Type 'c' to convert from 'Celsius' or 'f' to convert from 'Farenheit': ")
+if choice == 'c':
+    c_temp = float(input('Enter a temperature in Celsius: '))
+    f_temp = c_temp * 9 / 5 + 32
+    print(f_temp)
+
+elif choice == 'f':
+    f_temp = float(input('Enter a temperature in Celsius: '))
+    c_temp = (f_temp - 32) * 5 / 9
+    print(c_temp)
+
+else:
+    print('Invalid input -- please choose the conversion you want')
