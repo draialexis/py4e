d30e39cc32f4ab36ae9835e14a6d5ce27a586b24
Alexis DRAI <drai.alexis@gmail.com>
2021-07-06 14:32:52 +0200

    Add score.py to play with chained conditions

---
+++ score.py
@@ -0,0 +1,19 @@
+inp = input('Enter a score between 0.0 and 1.0: ')
+err_msg = 'Bad score'
+try:
+    score = float(inp)
+except:
+    print(err_msg)
+    quit()
+if score > 1.0 or score < 0.0:
+    print(err_msg)
+elif score >= 0.9:
+    print('A')
+elif score >= 0.8:
+    print('B')
+elif score >= 0.7:
+    print('C')
+elif score >= 0.6:
+    print('D')
+else:
+    print('F')
