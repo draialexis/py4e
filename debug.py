2212490e10525f494e3a6b47cc87be16a6fa02b6
Alexis DRAI <drai.alexis@gmail.com>
2021-07-20 19:28:10 +0200

    Combine two conditional statements

--- debug.py
+++ debug.py
@@ -3,6 +3,6 @@ count = 0
 for line in fhand:
     words = line.split()
     # print('Debug:', words)
-    if len(words) == 0 : continue
-    if words[0] != 'From' : continue
+    if len(words) == 0 or words[0] != 'From':
+        continue
     if len(words) >= 3 : print(words[2])
