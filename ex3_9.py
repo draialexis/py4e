3066cf81c8af51395f15f895eedf4b2955851ecb
Alexis DRAI <drai.alexis@gmail.com>
2021-08-11 23:17:16 +0200

    Add program to categorize email addresses of mailbox log

---
+++ ex3_9.py
@@ -0,0 +1,19 @@
+fname = input('Enter a file name: ')
+furl = 'text_docs\\' + fname + '.txt'
+
+try:
+    fhand = open(furl)
+except:
+    print('nah-aah')
+    exit()
+
+emails = dict()
+
+for line in fhand:
+    if line.startswith('From'):
+        words = line.split()
+        if len(words) >= 2:
+            email = words[1]
+            emails[email] = emails.get(email, 0) + 1
+
+print(emails)
