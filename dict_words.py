ffc86e3c9800bcb6f05345daeadbbd131a8897fa
Alexis DRAI <drai.alexis@gmail.com>
2021-08-11 22:56:10 +0200

    Make the parsing more complex
    Removing punctuation, white space, uppercase, etc.

--- dict_words.py
+++ dict_words.py
@@ -1,3 +1,5 @@
+import string
+
 fname = input('Enter a file name: ')
 furl = 'text_docs\\' + fname + '.txt'
 
@@ -10,6 +12,16 @@ except:
 words = dict()
 
 for line in fhand:
+    line = line.rstrip()
+    line = line.lower()
+    line = line.translate(line.maketrans('', '', string.punctuation))
     for word in line.split():
         words[word] = words.get(word, 0) + 1
 print(words)
+
+vals = list(words.values())
+max_val = max(vals)
+
+for word, count in words.items():
+    if count == max_val:
+        print('Most frequent word:', word, '~~', count)
