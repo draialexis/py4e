14a2a628d55d305b29c7064c978bec08c713c79b
Alexis DRAI <drai.alexis@gmail.com>
2021-08-11 23:56:01 +0200

    Add most frequent correspondent

---
+++ ex4_9.py
@@ -0,0 +1,24 @@
+fname = input('Enter a file name: ')
+furl = 'text_docs\\' + fname + '.txt'
+
+try:
+    fhand = open(furl)
+except:
+    print('nah-aah')
+    exit()
+
+emails = dict()
+
+for line in fhand:
+    if line.startswith('From'):
+        words = line.split()
+        if len(words) >= 2:
+            email = words[1]
+            emails[email] = emails.get(email, 0) + 1
+
+print(emails)
+
+max_count = max(list(emails.values()))
+for email, count in emails.items():
+    if count == max_count:
+        print('Most frequent correspondent:', email, '(count', count, ')')
