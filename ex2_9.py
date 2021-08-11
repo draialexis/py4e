84169e5ae393d0fe488731791102dec965b61451
Alexis DRAI <drai.alexis@gmail.com>
2021-08-11 23:12:03 +0200

    Add program to categorize weekdays of mailbox log

---
+++ ex2_9.py
@@ -0,0 +1,19 @@
+fname = input('Enter a file name: ')
+furl = 'text_docs\\' + fname + '.txt'
+
+try:
+    fhand = open(furl)
+except:
+    print('nah-aah')
+    exit()
+
+days = dict()
+
+for line in fhand:
+    if line.startswith('From'):
+        words = line.split()
+        if len(words) >= 3:
+            day = words[2]
+            days[day] = days.get(day, 0) + 1
+
+print(days)
